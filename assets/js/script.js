0f889d06f77c48b8ad7716d5d60fe5b58e76c51b
AMJL-16 <ozaurelio@gmail.com>
2023-03-16 16:13:12 +0000

    Add some js and fixed some html

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choices
+ */
+/*const buttons = document.getElementsByClassName("select");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const messages = document.getElementById("messages");
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+
+
+
+
+ // addEventListeners to the buttons
+for (let button of buttons) {
+  button.addEventListener('click', function() {
+    let userChoice = this.getAttribute('data-choice');
+    playGame(userChoice);
+  })
+}
+
+// add event listener and setTimeout() to rules button.
+/*function myFunction() {
+  document.getElementById("rule-text").innerHTML = "A Simple Way to Remember Who Wins:Scissors cut paper|Paper covers rock|Rock crushes lizard|Lizard poisons Spock|Spock smashes scissors|Scissors decapitate the lizard|Lizard eats paper|Paper disproves Spock|Spock vaporizes rock|Rock crushes scissors|";
+  setTimeout(function() {
+    document.getElementById("rule-text").style.display = "none";
+  }, 15000); // I set the timeOutfunction to 15 seconds to have the rules disappear.
+}*/
