ce2e0027c1de25ab6232a503b0e035fc536283ee
AMJL-16 <ozaurelio@gmail.com>
2023-03-17 18:21:08 +0000

    Add global variables for DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,55 @@
 /**
- * Declare constants for DOM elements
- * and possible choices
+ * Declare Global constants for DOM elements
  */
-const buttons = document.getElementsByClassName("game");
+const userChoice = document.getElementById("your-choice");
+const computerchoice = document.getElementById("machine-choice");
 const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
+const computerScore = document.getElementById("computer-score"); 
+const choices = document.querySelectorAll("button"); // using the queryselectorall function to selsect all the buttons.
+
+let playerMove
+let computerMove
+/** 
+* use the for each method and addEventListeners to the buttons using
+* the arrow method to pass trough a function
+*/ 
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+  playerMove = e.target.id 
+  userChoice.innerHTML = playerMove
+  generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 4)
+  
+  if (randomChoice === 0) {
+    computerMove = 'rock'
+  }
+  if (randomChoice === 1) {
+    computerMove = 'paper'
+  }
+  if (randomChoice === 2) {
+    computerMove = 'scissors'
+  }
+  if (randomChoice === 3) {
+    computerMove = 'lizard'
+  }
+  if (randomChoice === 4) {
+    computerMove = 'spock'
+  }
+  computerchoice.innerHTML = computerMove
+}
 
 
 
 
 
-
-
- // addEventListeners to the buttons
 
 
 // add event listener and setTimeout() to rules button.
-function myFunction() {
+/**function myFunction() {
   document.getElementById("rule-text").innerHTML = "A Simple Way to Remember Who Wins:Scissors cut paper|Paper covers rock|Rock crushes lizard|Lizard poisons Spock|Spock smashes scissors|Scissors decapitate the lizard|Lizard eats paper|Paper disproves Spock|Spock vaporizes rock|Rock crushes scissors|";
   setTimeout(function() {
     document.getElementById("rule-text").style.display = "none";
   }, 15000); // I set the timeOutfunction to 15 seconds to have the rules disappear.
-}
+}*/
